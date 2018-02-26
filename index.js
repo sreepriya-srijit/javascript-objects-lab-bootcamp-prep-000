d5ff28b31db5478e3e9222961a5705ee02ba6d2c
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-02-26 23:58:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {prop1: 1}
 
 function updateObjectWithKeyAndValue(object,key, value){
-  var copy = Object.assign({}, object,recipes, {key: value})
-  return copy
+  var newObj = Object.assign({}, object,recipes, {key: value})
+  return newObj
 }
